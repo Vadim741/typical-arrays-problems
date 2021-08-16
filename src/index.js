84f09ac2e0aa32dfdd952a38ffa95acfdcdcb80d
Vadim <tutwadimka@gmail.com>
2021-08-16 19:25:05 +0300

    fix: add js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.sort (function(a, b) { return a - b; });
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.sort (function(a, b) { return a - b; });
+        return array[array.length - 1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++){
+            avg += array[i];
+        }
+        return (avg/array.length)
+    }
 }
